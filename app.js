6676eb251110e255612aa435a13535f7e9fea11b
Paulo Morais <paulo@viraminas.org.br>
2023-04-12 17:22:23 -0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,33 @@
+// Define a function to fetch the Markdown file
+function fetchMarkdownFile() {
+  return fetch('blog-post.md')
+    .then(response => response.text())
+    .then(text => parseMarkdownFile(text));
+}
+
+// Define a function to parse the YAML front matter and content of the Markdown file
+function parseMarkdownFile(text) {
+  const parts = text.split('---');
+  const frontMatter = parts[1].trim();
+  const content = parts.slice(2).join('---').trim();
+  const data = YAML.parse(frontMatter);
+  return { data, content };
+}
+
+// Define a function to render the Markdown content
+function renderMarkdownContent({ data, content }) {
+  const markdown = new markdownit();
+  const html = markdown.render(content);
+  const title = data.title || 'Untitled';
+  const date = data.date || 'Unknown Date';
+  const author = data.author || 'Unknown Author';
+  const template = `
+    <h1>${title}</h1>
+    <p>${date} by ${author}</p>
+    ${html}
+  `;
+  document.getElementById('content').innerHTML = template;
+}
+
+// Fetch the Markdown file and render the content
+fetchMarkdownFile().then(renderMarkdownContent);
